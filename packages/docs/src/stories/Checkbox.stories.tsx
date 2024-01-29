1bb30551f2d647ee73d4b32d194be3a04adf6c90
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-01-29 18:16:32 -0300

    Componente: Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,29 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+/**
+ * Component global que serve para aplicar todas as configurações para todos components
+ */
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Aceept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+/**
+ * component de botão Primary todas as configurações de um botão Primary deve ser feito no mesmo
+ */
+export const Primary: StoryObj<CheckboxProps> = {}
---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { Check } from 'phosphor-react'
+
+export type CheckboxProps = ComponentProps<typeof CheckboxContainer>
+
+export const Checkbox = (props: CheckboxProps) => {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
