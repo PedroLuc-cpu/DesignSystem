c7c8ca78fa0f6910814899a5fa95f5aa6cb7e9c8
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-01-28 22:02:07 -0300

    Componente: TextInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,46 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+/**
+ * Component global que serve para aplicar todas as configurações para todos components
+ */
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+/**
+ * component de botão Primary todas as configurações de um botão Primary deve ser feito no mesmo
+ */
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+    placeholder: 'Type your name',
+  },
+}
