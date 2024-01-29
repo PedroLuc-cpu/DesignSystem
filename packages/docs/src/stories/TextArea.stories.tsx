6797eea5aaa421db11717545985d96cd226985e5
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-01-29 17:44:18 -0300

    Componente: TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,39 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+
+/**
+ * Component global que serve para aplicar todas as configurações para todos components
+ */
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+/**
+ * component de botão Primary todas as configurações de um botão Primary deve ser feito no mesmo
+ */
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Type your observations',
+  },
+}
+
+export const Desabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
