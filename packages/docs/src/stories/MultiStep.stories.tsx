70ed68f19cd9eb4f7dba9c8393e94c53699a0a25
pedroluc-cpu <pedrolucas_gta2015@hotmail.com>
2024-01-31 18:05:28 -0300

    Componente: MultiStep

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,39 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+
+/**
+ * Component global que serve para aplicar todas as configurações para todos components
+ */
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+/**
+ * component de botão Primary todas as configurações de um botão Primary deve ser feito no mesmo
+ */
+export const Primary: StoryObj<MultiStepProps> = {
+  args: {},
+}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
